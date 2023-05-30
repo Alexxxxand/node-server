faaae5693ecfb6d753a826e04071cfbe7271a6c9
Alex <95047525+Alexxxxand@users.noreply.github.com>
2023-05-30 23:00:03 +0300

    server

---
+++ server.js
@@ -0,0 +1,19 @@
+//modules (ejs, express)
+const express = require ('express');
+const app = express();
+
+//settings ( in the static folder you need to move the css, js, photo files and the rest of the media file )
+app.set('view engine', 'ejs');
+app.use(express.static('static'));
+
+//page (Be sure to change the file extension from html to ejs)
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
+//starting site (in PORT you can choose the port)
+const PORT = "80";
+
+app.listen(PORT, () => {
+    console.log ('Started')
+});
